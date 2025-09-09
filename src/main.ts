a3b4d090bca1e6cd81dc43bf1ed8968f469a330d
Ihury <ihury@biomainvestimentos.com>
2025-09-09 19:37:56 -0300

    chore: add prisma ORM

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ouve SIGINT/SIGTERM e dispara OnModuleDestroy/OnApplicationShutdown
+  // Importante pra que o PrismaService desconecte do banco ao finalizar a aplicação
+  await app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
